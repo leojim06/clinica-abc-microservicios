26258e70a40024bcc666ab665a12d44b2b44190d
Oscar Jimenez <o.jimenezm@uniandes.edu.co>
2021-09-12 13:52:25 -0500

    agregar función echo

--- microservice_registrar_paciente/app.py
+++ microservice_registrar_paciente/app.py
@@ -3,6 +3,7 @@ from .modelos import db
 from flask_restful import Api
 from .vistas import VistaRegistrarPaciente
 import requests
+from flask import jsonify
 
 
 app = create_app('default')
@@ -15,3 +16,7 @@ db.create_all()
 api = Api(app)
 api.add_resource(VistaRegistrarPaciente, '/paciente')
 
+@app.route('/echo/<nodo>', methods=['GET'])
+def echo(nodo=None):
+    data = {'estado: ' + nodo: 'ok'}
+    return jsonify(data), 200
