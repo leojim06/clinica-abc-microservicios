2d5dfc4824366bbbe8cac81f6e7018908203ecc8
Daniel <daniquice.developer@gmail.com>
2021-10-01 19:21:14 -0500

    validador de hash

--- microservice_generar_hash/flaskr/app.py
+++ microservice_generar_hash/flaskr/app.py
@@ -1,5 +1,4 @@
 from flaskr import create_app
-from .modelos import db
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from .vistas import VistaGenerarHash
@@ -7,11 +6,6 @@ app = create_app('default')
 app_context = app.app_context()
 app_context.push()
 
-db.init_app(app)
-db.create_all()
-
 api = Api(app)
-api.add_resource(VistaGenerarHash, '/generarHash')
-
-jwt = JWTManager(app)
+api.add_resource(VistaGenerarHash, '/validador-hash')
 
--- microservice_generar_hash/flaskr/vistas/vistas.py
+++ microservice_generar_hash/flaskr/vistas/vistas.py
@@ -1,14 +1,36 @@
 from flask_restful import Resource
 from flask import request
 import hashlib
-
+import base64
+import hmac
+import json
 
 
 class VistaGenerarHash(Resource):
+    
     def post(self):
-        texto = request.json["texto"]
-        textoMD5 = hashlib.md5()
-        textoMD5.update(texto.encode("UTF-8"))
-        textoMD5 = textoMD5.hexdigest()
-        return textoMD5
+
+        httpsig = request.headers.get('Httpsig')
+        current_Date = request.headers.get('Current-Date')
+        target_Url = request.headers.get('Target-Url')
+        a1 = dict(item.split('=',1) for item in httpsig.split(','))
+        signature = a1['signature'].replace('"', '')
+        hash_body = VistaGenerarHash.calcular_hash_body(request.json)
+        signature_calculated = VistaGenerarHash.calcular_signature(target_Url, current_Date, hash_body)
+        
+        if signature == signature_calculated:
+            return {}, 200
+        
+        return {}, 400
+
+    def calcular_signature(target_url, date, digest):
+        cadena = "(request-target): {target_url}\ndate: {date}\ndigest: {digest}".format(target_url=target_url, date=date, digest=digest)
+        hmacsha256 = hmac.new(key=bytes("abcdefghijk", 'latin-1'), msg=cadena.encode(), digestmod=hashlib.sha256).digest()
+        hmacbase64 = base64.b64encode(hmacsha256)
+        return hmacbase64.decode('utf-8')
+
+    def calcular_hash_body(body):
+        s = json.dumps(body, separators=(',', ':'))
+        result = base64.b64encode(hashlib.sha256(s.encode()).digest())
+        return result.decode('utf-8')
 
